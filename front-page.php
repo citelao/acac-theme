f7ab8305cc1dda31f9f637469c1688e3ae876a11
Ben <ben@stolovitz.com>
2016-07-30 20:37:39 -0700

    hackey full-width bars

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <header class="header">
 	<h1 class="header__logo logo">
-		<a href="/" class="logo__link">
+		<a href="/" class="logo__link" title="self-link to ACAC homepage">
 			<img class="logo__image" src="<?php bloginfo('template_directory'); ?>/assets/images/logo.svg" alt="the ACAC logo">
 		</a>
 	</h1>
@@ -12,33 +12,39 @@
 </header>
 
 <section class="section">
-	<div class="section__content">
-		<h2>We run auditions.</h2>
-		<p>
-			ACAC makes sure that auditions are <strong>friendly, fair, and fun</strong>.
-			We handle the logistics, you handle your solo.
-		</p>
-		<a href="" class="button button--full button--emphasis">audition procedures</a>
-		<a href="" class="button button--full">groups</a>
-		<!-- <a href="" class="button button--full">find us</a> -->
+	<div class="section__wrapper">
+		<div class="section__content">
+			<h2>We run auditions.</h2>
+			<p>
+				ACAC makes sure that auditions are <strong>friendly, fair, and fun</strong>.
+				We handle the logistics, you handle your solo.
+			</p>
+			<a href="" class="button button--full button--emphasis">audition procedures</a>
+			<a href="" class="button button--full">groups</a>
+			<!-- <a href="" class="button button--full">find us</a> -->
+		</div>
 	</div>
 </section>
 
 <section class="section section--dark">
-	<div class="section__content">
-		<h2>What is ACAC?</h2>
-		<p>Twelve a cappella groups at one school can be a lot. That’s why, in 2005, we founded ACAC to be the face for a cappella at WUSTL.</p>
-		<a href="">Learn more &rarr;</a>
+	<div class="section__wrapper">
+		<div class="section__content">
+			<h2>What is ACAC?</h2>
+			<p>Twelve a cappella groups at one school can be a lot. That’s why, in 2005, we founded ACAC to be the face for a cappella at WUSTL.</p>
+			<a href="">Learn more &rarr;</a>
+		</div>
 	</div>
 </section>
-
+<!-- 
 <section class="section section--dark">
-	<div class="section__content">
-		<h2>What is ACAC?</h2>
-		<p>Twelve a cappella groups at one school can be a lot. That’s why, in 2005, we founded ACAC to be the face for a cappella at WUSTL.</p>
-		<a href="">Learn more &rarr;</a>
+	<div class="section__wrapper">
+		<div class="section__content">
+			<h2>What is ACAC?</h2>
+			<p>Twelve a cappella groups at one school can be a lot. That’s why, in 2005, we founded ACAC to be the face for a cappella at WUSTL.</p>
+			<a href="">Learn more &rarr;</a>
+		</div>
 	</div>
-</section>
+</section> -->
 
 <section class="section">
 	<div class="section__content">
